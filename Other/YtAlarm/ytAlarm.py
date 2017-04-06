ea154c91918c1fb42863dd2a200001b91a7c7d8e
Schmidt23 <crs.business.bro@googlemail.com>
2017-04-06 16:09:30 +0200

    optimized yt_alarm script

--- Other/YtAlarm/ytAlarm.py
+++ Other/YtAlarm/ytAlarm.py
@@ -13,25 +13,26 @@ alarm = (hour, minute)
 
 t = time.localtime()
 now = t.tm_hour, t.tm_min
+flag = False
 
 #check if time == alarm if not keep in loop
-while (alarm != now):
+while (not flag):
     #update time
     t = time.localtime()
     now = t.tm_hour, t.tm_min
-    print "Time: ",time.strftime("%H:%M"), "Alarm: ","%d:%d" %(hour, minute)
-    time.sleep(15)
-
-#if out of loop
-if alarm == now:
-    #open file, get random link and open in webbrowser
-    content = 'content.txt'
-    x = open(content, 'r')
-    line = x.readlines()
-    randomLink = random.choice(line)
-    x.close()
-    webbrowser.open(randomLink)
-
+    print "Time: ",time.strftime("%H:%M"), "Alarm: ","%02d:%02d" %(hour, minute)
+    if alarm != now:
+        time.sleep(15)
+    elif alarm == now:
+        #open file, get random link and open in webbrowser
+        #AND SET FLAG !!!!!!!!!!!
+        content = 'content.txt'
+        x = open(content, 'r')
+        line = x.readlines()
+        randomLink = random.choice(line)
+        x.close()
+        webbrowser.open(randomLink)
+        flag = True
 
 
 
