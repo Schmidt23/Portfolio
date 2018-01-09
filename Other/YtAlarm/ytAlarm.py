ac2ed7b07a63b5a31d6e2185bd013ee211a0aba5
Schmidt23 <crs.business.bro@googlemail.com>
2018-01-09 11:27:13 +0100

    Update ytAlarm.py
    time is now displayed by digital clock format
    open_link has to be retested but should still work 
    #TODO: tidy up and -maybe- optimize

--- Other/YtAlarm/ytAlarm.py
+++ Other/YtAlarm/ytAlarm.py
@@ -4,35 +4,57 @@
 import webbrowser
 import random
 import time
+import random 
+import os
 
+# time is displayed as digital clock 
+digital = {"1":["   ","  |", "  |"], "2":[" _ "," _|", "|_ "], "3":[" _ ", " _|", " _|"], 
+	"4":["   ","|_|","  |"],"5":[" _ ", "|_ ", " _|"], "6":[" _ ", "|_ ", "|_|"], "7":[" _ ", "  |", "  |"],
+	"8":[" _ ", "|_|", "|_|"],"9":[" _ ", "|_|", "  |"], "0":[" _ ","| |","|_|"], ":":["   ", " o ", " o "],
+	" ":["   ", "   ", "   "]
+}
+
+
+def display(clock):
+	for i in xrange(3):
+		for j in clock:
+			print digital[j][i]+"\f",
+		print
+
+
+def open_link():
+	print "opening"
+	#open file, get random link and open in webbrowser
+	content = 'content.txt'
+	x = open(content, 'r')
+	line = x.readlines()
+	randomLink = random.choice(line)
+	x.close()
+	webbrowser.open(randomLink)
 
 setTime = raw_input("Alarm? ")
 #split input into tuple of hour and minutes
 hour, minute = int(setTime[:2]), int(setTime[2:])
 alarm = (hour, minute)
 
-t = time.localtime()
-now = t.tm_hour, t.tm_min
+
 flag = False
 
-#check if time == alarm if not keep in loop
+	#check if time == alarm if not keep in loop
 while (not flag):
-    #update time
-    t = time.localtime()
-    now = t.tm_hour, t.tm_min
-    print "Time: ",time.strftime("%H:%M"), "Alarm: ","%02d:%02d" %(hour, minute)
-    if alarm != now:
-        time.sleep(15)
-    elif alarm == now:
-        #open file, get random link and open in webbrowser
-        #AND SET FLAG !!!!!!!!!!!
-        content = 'content.txt'
-        x = open(content, 'r')
-        line = x.readlines()
-        randomLink = random.choice(line)
-        x.close()
-        webbrowser.open(randomLink)
-        flag = True
+	#update time
+	os.system('clear')
+	now = time.localtime()
+	clock = "%02d" % now.tm_hour +":"+ "%02d" % now.tm_min
+	display(clock)
+	if alarm != (now.tm_hour,now.tm_min):
+		time.sleep(15)
+	elif alarm == (now.tm_hour,now.tm_min):
+		print "wat"
+		open_link()
+		#set Flag to stop loop
+		flag = True
+
 
 
 
