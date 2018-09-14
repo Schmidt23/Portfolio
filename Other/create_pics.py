ec5157a66ebba0836c0c53da61f6d3bffc66c7b6
Schmidt23 <crs.business.bro@googlemail.com>
2018-09-14 12:06:05 +0200

    changed string formatting; changed description

--- Other/create_pics.py
+++ Other/create_pics.py
@@ -1,7 +1,7 @@
 # ! encoding=utf-8
 
 """Create a given number of pictures increasing in size by 1 MB. File format can be changed from TIFF but only TIFF works
-as intended atm since there is no compression. Accurate until 43 because a few too many KBs get added"""
+as intended atm since there is no compression. (Somehow now) Pretty Accurate at least 'til 100 MB'"""
 
 from PIL import Image, ImageDraw, ImageFont
 import random
@@ -20,7 +20,7 @@ def create_pic(name, w, h, fm, ind, pth):
         pxdata.append(random.choice(pixels))
     im.putdata(pxdata)
     try:
-        im.save("%s\\%s%d%s" % (pth, name, ind, fm))
+        im.save("{}\\{}{}{}".format(pth, name, ind, fm))
     except IOError as e:
         print e
         sys.exit(1)
@@ -35,7 +35,7 @@ def add_multiple(li, it, n):
 
 def fast_create_pic(name, w, h, fm, ind, pth):
     # creates file with 100*100 px stretched to given size --faster
-    pixels = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 0, 0), (255, 255, 255)]
+    pixels = [(255, 0, 0), (0, 255, 0), (0, 0, 255)] #(0, 0, 0), (255, 255, 255)]
     pxdata = []
 
     im = Image.new('RGB', (100, 100))
@@ -44,7 +44,7 @@ def fast_create_pic(name, w, h, fm, ind, pth):
     im.putdata(pxdata)
     im = im.resize((w, h))
     try:
-        im.save("%s\\%s%d%s" % (pth, name, ind, fm))
+        im.save("{}\\{}{}{}".format(pth, name, ind, fm))
     except IOError as e:
         print e
         sys.exit(1)
@@ -57,9 +57,9 @@ def text_create_pic(name, w, h, fm, ind, pth, txt):
     # creates pic with given text. Needs link to font!!!
     im = Image.new('RGB', (w, h), (255, 255, 255))
     d = ImageDraw.Draw(im)
-    d.text((10, 10), txt + "\n%d" % ind, font=fnt, fill=(0, 0, 0))
+    d.text((10, 10), '{!s} \n{}'.format(txt, ind), font=fnt, fill=(0, 0, 0))
     try:
-        im.save("%s\\%s%d%s" % (pth, name, ind, fm))
+        im.save("{}\\{}{}{}".format(pth, name, ind, fm))
     except IOError as e:
         print e
         sys.exit(1)
@@ -72,9 +72,9 @@ def just_text(name, fm, ind, pth, txt):
     # creates files containing text with fixed size
     im = Image.new('RGB', (591, 591), (255, 255, 255))
     d = ImageDraw.Draw(im)
-    d.text((10, 10), txt + "\n%d" % ind, font=fnt, fill=(0, 0, 0))
+    d.text((10, 10), '{!s} \n{}'.format(txt, ind), font=fnt, fill=(0, 0, 0))
     try:
-        im.save("%s\\%s%d%s" % (pth, name, ind, fm))
+        im.save("{}\\{}{}{}".format(pth, name, ind, fm))
     except IOError as e:
         print e
         sys.exit(1)
@@ -87,25 +87,26 @@ def return_size(idx):
     # returns size/dimensions needed to create idx+1.MB file
     # 1MB(1024² byte) / 3 (3byte per RGB pixel) == w*h if w==h
     mb = (idx) * MB
-    return int(math.sqrt(mb / BYTES))
+    return int(math.sqrt(mb / BYTES))+1
 
 
 def return_text(fl):
+    #tries to open given string as path to file. Returns input as string if path not valid.
     try:
         with open(fl,'rb') as f:
             text = f.read()
     except IOError:
-        print "%s is not a valid file path. Input will be used as string." %fl
+        print "{} is not a valid file path. Input will be used as string.".format (fl)
         text = fl
     return text
 
 if __name__ == "__main__":
     # constants
-    MB = 1024 * 1024.00
+    MB = 1024 * 1024.00000
     BYTES = 3
     # default values
     name_pic = "pic"
-    nr_pics = 1
+    nr_pics = 3
     file_format = ".tif"
     func = create_pic
     path = os.getcwd()
@@ -143,18 +144,18 @@ if __name__ == "__main__":
         elif opt == '-n':
             nr_pics = int(arg)
             if not one_file:
-                print "number of pics to create: ", nr_pics
+                print "number of pics to create: {}".format(nr_pics)
             else:
-                print "size of file: ~%d MB" % nr_pics
+                print "size of file: ~{}MB".format(nr_pics)
         elif opt == '-f':
             file_format = arg
-            print "file format changed to %s. Please be aware that file sizes will probably be off the mark" % arg
+            print "file format changed to {}. Please be aware that file sizes will probably be off the mark".format(arg)
         elif opt == "-p":
             path = arg
-            print "path changed to", path
+            print "path changed to {}".format(path)
         elif opt == '-i':
             name_pic = arg
-            print "filename changed to ", arg
+            print "filename changed to {}".format(arg)
 
     if not one_file:
         if func == text_create_pic:
@@ -166,23 +167,23 @@ if __name__ == "__main__":
         elif func == just_text:
             for i in xrange(1,nr_pics+1):
                 just_text(name_pic, file_format, i, path, txt)
-                print path + "\\pic" + str(i)
+                print "{}\\pic{}".format(path,str(i))
         else:
             for i in xrange(1,nr_pics+1):
                 wh = return_size(i)
                 func(name_pic, wh, wh, file_format, i, path)
-                print path + "\\pic" + str(i), wh, "  ", wh ** 2 * BYTES / MB
+                print "{}\\pic{} {}  {:.5f}".format(path, str(i), wh, (wh ** 2 * BYTES / MB))
     else:
         if func == text_create_pic:
             wh = return_size(nr_pics)
             text_create_pic("of"+name_pic, wh, wh, file_format, nr_pics, path, txt)
-            print path + "\\pic" + str(nr_pics)
+            print "{}\\pic{}".format(path,str(nr_pics))
 
         elif func == just_text:
             print "just text"
             just_text("of"+name_pic, file_format, nr_pics, path, txt)
-            print path + "\\pic" + str(nr_pics)
+            print "{}\\pic{}".format(path,str(nr_pics))
         else:
             wh = return_size(nr_pics)
             func("of"+name_pic, wh, wh, file_format, nr_pics, path)
-            print path + "\\pic" + str(nr_pics), wh, "  ", wh ** 2 * BYTES / MB
+            print "{}]\\pic{} {}  {:.5f}".format(path,str(nr_pics), wh,(wh ** 2 * BYTES / MB))
