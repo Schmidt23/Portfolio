ae2dff49496198be9a5fb32ab041cf53e39876bf
Schmidt23 <crs.business.bro@googlemail.com>
2026-03-06 12:56:29 +0100

    setup poetry, ruff, black, pre-commit and updated files

--- DSA_Dice/main.py
+++ DSA_Dice/main.py
@@ -6,12 +6,15 @@ import json
 import random
 import logging
 
+logger = logging.getLogger(__name__)
+
 
 def setup_logging(level):
     logging.basicConfig(
-        level=level,      
+        level=level,
         format="%(asctime)s [%(levelname)s] %(message)s",
     )
+
 
 def load_character(name):
     if not hasattr(load_character, "_cache"):
@@ -20,12 +23,12 @@ def load_character(name):
         return load_character._cache[name]
     try:
         with open(f"{name}.json") as f:
-            data =  json.load(f)
+            data = json.load(f)
     except FileNotFoundError as e:
         raise FileNotFoundError(f"Character file {name}.json not found") from e
     except json.JSONDecodeError as e:
         raise ValueError(f" {name}.json invalid") from e
-    
+
     load_character._cache[name] = data
     return data
 
@@ -36,87 +39,95 @@ def load_skills():
             with open("skill_list.json") as f:
                 load_skills._cache = json.load(f)
         except FileNotFoundError as e:
-            raise FileNotFoundError(f"Skill file .skill_list.json not found") from e
+            raise FileNotFoundError("Skill file .skill_list.json not found") from e
         except json.JSONDecodeError as e:
-            raise ValueError(f"skill file is invalid") from e
+            raise ValueError("skill file is invalid") from e
     return load_skills._cache
 
 
 def roll_dice():
-    rolls = [random.randint(1,20) for roll in range(3)]
+    rolls = [random.randint(1, 20) for roll in range(3)]
     return rolls
 
+
 def success_rate(attributes, points):
-    total = 8000.0 # 20**3
+    total = 8000.0  # 20**3
     success = 0
-    for d1 in range(1,21):
-        for d2 in range(1,21):
-            for d3 in range(1,21):
+    for d1 in range(1, 21):
+        for d2 in range(1, 21):
+            for d3 in range(1, 21):
                 rolls = [d1, d2, d3]
-                if rolls.count(1)>=2:
+                if rolls.count(1) >= 2:
                     success += 1
-                    continue
-                
-                if rolls.count(20)>=2:
                     continue
 
-                diff = [max(0, roll - attribute) for roll, attribute in zip(rolls, attributes)]
+                if rolls.count(20) >= 2:
+                    continue
+
+                diff = [
+                    max(0, roll - attribute)
+                    for roll, attribute in zip(rolls, attributes)
+                ]
                 if sum(diff) <= points:
                     success += 1
-    logging.info(f"{points}")
-    logging.info(f"{success}")
-    return success/total*100
+    logger.info(f"{points}")
+    logger.info(f"{success}")
+    return success / total * 100
+
 
 def get_values(character, skill_list, skill):
     try:
         skillpoints = int(character["Skillpoints"][skill])
         skill_parts = skill_list[skill]
-    except KeyError as e:                           
+    except KeyError as e:
         raise KeyError(f"Skill not found: {e}") from e
     except ValueError as e:
-        raise ValueError(f"Skill points must be integers") from e
+        raise ValueError("Skill points must be integers") from e
     except TypeError as e:
-        raise TypeError(f"Skill has invalid Type") from e
-    
+        raise TypeError("Skill has invalid Type") from e
+
     try:
         values = [int(character["Attributes"][attr]) for attr in skill_parts]
-    except KeyError as e:                           
+    except KeyError as e:
         raise KeyError(f"Attribut not found: {e}") from e
     except ValueError as e:
-        raise ValueError(f"Attributes must be integers") from e
+        raise ValueError("Attributes must be integers") from e
     except TypeError as e:
-        raise TypeError(f"Attributes has invalid Type") from e
+        raise TypeError("Attributes has invalid Type") from e
     return values, skillpoints
 
-def resolve_roll(attribute_values, rolls, skill_points):
-    logging.info(f"attribute_values={attribute_values}, rolls={rolls}, points={skill_points}")
 
-    #any value 0 or under, roll not permitted
-    if any(value<=0 for value in attribute_values):
+def resolve_roll(attribute_values, rolls, skill_points):
+    logger.info(
+        f"attribute_values={attribute_values}, rolls={rolls}, points={skill_points}"
+    )
+
+    # any value 0 or under, roll not permitted
+    if any(value <= 0 for value in attribute_values):
         return "Eigenschaft 0 oder negativ. Probe nicht erlaubt"
-    
-    compare = [value-roll for value, roll in zip(attribute_values, rolls)]
+
+    compare = [value - roll for value, roll in zip(attribute_values, rolls)]
     logging.debug(f"Über: {compare}")
 
-    roll_success = all(x>=0 for x in compare)
-    logging.debug(f"Erfolgreich: {roll_success}")
+    roll_success = all(x >= 0 for x in compare)
+    logger.debug(f"Erfolgreich: {roll_success}")
 
     remainder = None
 
-    if rolls.count(1) >=2:
-        return f"Kritischer Erfolg"
+    if rolls.count(1) >= 2:
+        return "Kritischer Erfolg"
     elif rolls.count(20) >= 2:
-        return f"Patzer"
+        return "Patzer"
     elif roll_success:
         return f"Sauber Gelungen Quali: {int((3+skill_points-1)/3)}"
     else:
         difference = [fail for fail in compare if fail < 0]
         remainder = skill_points + sum(difference)
-        logging.debug(f"Punkte über: {sum(difference)}, {skill_points}")
-        logging.info(f"Rest: {remainder}")
+        logger.debug(f"Punkte über: {sum(difference)}, {skill_points}")
+        logger.info(f"Rest: {remainder}")
 
     if remainder <= 2 and remainder >= 0:
-        return f"Gelungen Quali: 1 {remainder}"       
+        return f"Gelungen Quali: 1 {remainder}"
     elif remainder > 0:
         return f"Gelungen Quali: {int((3+remainder-1)/3)} {remainder}"
     else:
@@ -124,11 +135,12 @@ def resolve_roll(attribute_values, rolls, skill_points):
 
 
 def skill_check(character, skill_list, skill):
-    values, skillpoints = get_values(character,  skill_list, skill)
+    values, skillpoints = get_values(character, skill_list, skill)
     probability = success_rate(values, skillpoints)
-    logging.info(f"Erfolgswahrscheinlichkeit: {probability}")
+    logger.info(f"Erfolgswahrscheinlichkeit: {probability}")
     rolls = roll_dice()
-    return resolve_roll(values, rolls,  skillpoints)
+    return resolve_roll(values, rolls, skillpoints)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -138,13 +150,11 @@ def main():
     args = parser.parse_args()
 
     setup_logging(args.log_level.upper())
-    logger = logging.getLogger(__name__)
 
-
-    char_name = args.char 
+    char_name = args.char
     skill_name = args.skill
 
-    logging.info(f"Nutze Charakter '{char_name}' und Skill '{skill_name}'")
+    logger.info(f"Nutze Charakter '{char_name}' und Skill '{skill_name}'")
 
     character = load_character(char_name)
     skills = load_skills()
--- DSA_Dice/tests/test_resolve_roll.py
+++ DSA_Dice/tests/test_resolve_roll.py
@@ -1,109 +1,143 @@
 import pytest
 from main import resolve_roll
 
+
 @pytest.fixture
 def default_vals():
-    return [10,10,10]
+    return [10, 10, 10]
 
-@pytest.mark.parametrize("rolls",[[1,1,20],
-                                 [1,20,1],
-                                 [20,1,1],
-                                 [1,1,1], 
-], ids=["1,1,20", "1,20,1", "20,1,1", "1,1,1"])
+
+@pytest.mark.parametrize(
+    "rolls",
+    [
+        [1, 1, 20],
+        [1, 20, 1],
+        [20, 1, 1],
+        [1, 1, 1],
+    ],
+    ids=["1,1,20", "1,20,1", "20,1,1", "1,1,1"],
+)
 def test_crit_success(default_vals, rolls):
-    #arrange
+    # arrange
     vals = default_vals
     points = 5
 
-    #act
+    # act
     result = resolve_roll(vals, rolls, points)
 
-    #assert
+    # assert
     assert result == "Kritischer Erfolg"
 
-@pytest.mark.parametrize("rolls",[[1,20,20],
-                                  [20,20,1],
-                                  [20,1,20],
-                                  [20,20,20], 
-], ids=["1,20,20", "20,20,1", "20,1,20", "20,20,20"])
+
+@pytest.mark.parametrize(
+    "rolls",
+    [
+        [1, 20, 20],
+        [20, 20, 1],
+        [20, 1, 20],
+        [20, 20, 20],
+    ],
+    ids=["1,20,20", "20,20,1", "20,1,20", "20,20,20"],
+)
 def test_crit_fail(default_vals, rolls):
-    #arrange
+    # arrange
     vals = default_vals
     points = 25
 
-    #act
+    # act
     result = resolve_roll(vals, rolls, points)
 
-    #assert
+    # assert
     assert result == "Patzer"
 
-@pytest.mark.parametrize("vals",[[0,20,20],
-                                  [20,20,0],
-                                  [20,0,20],
-                                  [0,0,0],
-                                  [20,20,-3], 
-], ids=["0,20,20", "20,20,0", "20,0,20", "0,0,0", "20,20,-3"])
+
+@pytest.mark.parametrize(
+    "vals",
+    [
+        [0, 20, 20],
+        [20, 20, 0],
+        [20, 0, 20],
+        [0, 0, 0],
+        [20, 20, -3],
+    ],
+    ids=["0,20,20", "20,20,0", "20,0,20", "0,0,0", "20,20,-3"],
+)
 def test_impossible_roll(vals):
-    #arrange
-    rolls = [1,1,1]
+    # arrange
+    rolls = [1, 1, 1]
     points = 25
 
-    #act
+    # act
     result = resolve_roll(vals, rolls, points)
 
-    #assert
+    # assert
     assert result == "Eigenschaft 0 oder negativ. Probe nicht erlaubt"
 
 
-@pytest.mark.parametrize("points",[1,
-                                  3,
-                                  2,
-                                  0,
-                                  50, 
-], ids=["1", "3", "2", "0", "50"])
+@pytest.mark.parametrize(
+    "points",
+    [
+        1,
+        3,
+        2,
+        0,
+        50,
+    ],
+    ids=["1", "3", "2", "0", "50"],
+)
 def test_success_quality(default_vals, points):
-    #arrange
+    # arrange
     vals = default_vals
-    rolls = [9,9,9]
+    rolls = [9, 9, 9]
 
-    #act
+    # act
     result = resolve_roll(vals, rolls, points)
 
-    #assert
+    # assert
     assert result == f"Sauber Gelungen Quali: {int((3+points-1)/3)}"
 
 
-@pytest.mark.parametrize("points",[3,
-                                  4,
-                                  5,
-                                  6,
-], ids=["3", "4", "5", "6"])
+@pytest.mark.parametrize(
+    "points",
+    [
+        3,
+        4,
+        5,
+        6,
+    ],
+    ids=["3", "4", "5", "6"],
+)
 def test_success_quality_one(default_vals, points):
-    #arrange
+    # arrange
     vals = default_vals
-    rolls = [11,11,11]
+    rolls = [11, 11, 11]
 
-    #act
+    # act
     result = resolve_roll(vals, rolls, points)
 
-    #assert
+    # assert
     assert result == f"Gelungen Quali: 1 {points-3}"
 
 
-@pytest.mark.parametrize("points",[4,
-                                  7,
-                                  10,
-                                  13,
-                                  16,
-                                  19,
-], ids=["4", "7", "10", "13", "16", "19"])
+@pytest.mark.parametrize(
+    "points",
+    [
+        4,
+        7,
+        10,
+        13,
+        16,
+        19,
+    ],
+    ids=["4", "7", "10", "13", "16", "19"],
+)
 def test_success_quality_edge_cases(default_vals, points):
-    #arrange
+    # arrange
     vals = default_vals
-    rolls = [11,11,11]
+    rolls = [11, 11, 11]
 
-    #act
+    # act
     result = resolve_roll(vals, rolls, points)
 
-    #assert
+    # assert
     assert result == f"Gelungen Quali: {int((points-1)/3)} {points-3}"
